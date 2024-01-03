957624560528b090a936c88c209749b249f4f75a
Patumwanpang <pun_yavee@live.com>
2024-01-03 16:29:57 +0700

    add lab5

--- lab5/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        FuncInterface1 instance;
-
-        instance = () -> (int)(Math.random() * 1000);
-        System.out.println(instance.method1());
-    }
-}
